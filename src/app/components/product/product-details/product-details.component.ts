18fe698edaf839a16816a2f73cca8883e5fb56ca
William Mba <wmba@jovaco.ca>
2022-09-22 00:33:32 -0400

    add displaying product details

--- src/app/components/product/product-details/product-details.component.ts
+++ src/app/components/product/product-details/product-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/interfaces/product';
+import { products } from 'src/app/products';
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+  product: Product | undefined;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    // get the product id
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    // find the product that correspond with the provided id
+    this.product = products.find(product => product.id === productIdFromRoute);
   }
 
 }
